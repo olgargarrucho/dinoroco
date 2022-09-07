da2aad4b5fdf5a173edd6ad79a0e2e3061fa9f77
Olga RG <olga_1847@hotmail.com>
2022-09-07 22:01:27 +0200

    Added link to home at logo from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import '../styles/page/header.scss';
 import logo from '../images/logo.png';
+import { Link } from 'react-router-dom';
 
 function Header () {
     return (
         <header className='header'>
-            <img src={logo} className='header__logo' alt='Logo de Dinoroco SL' name='Logo de Dinoroco SL'/>
+            <Link to='/' ><img src={logo} className='header__logo' alt='Logo de Dinoroco SL' name='Logo de Dinoroco SL'/></Link>
         </header>
     )
 }
